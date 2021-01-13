d195513be2bc9b87121dda8e4e6359e28c242c51
sudhanshu-aggarwal <sudhanshu-aggarwal>
2021-01-13 09:58:01 +0530

    Edit Button Fixed

--- To-Do.js
+++ To-Do.js
@@ -1,13 +1,39 @@
 $(document).ready(function () {
     // var edt;
 
+    function edit() {
+
+        var txt1 = $(this).prev().prev();
+        var int = $(this).prev();
+
+        console.log(txt1.text());
+        console.log(int.val());
+        if (int.val() == "") {
+            int.val(txt1.text());
+
+        }
+        else {
+            txt1.text(int.val());
+
+            int.val("");
+        }
+
+
+    };
+
+
     $(".at").click(function () {
         var txt = $("#floatingInput").val();
         var lst;
         if (txt != "") {
-
-            lst = $('<li class="list-group-item lis"><input class="form-check-input me-2 mt-2" type="checkbox" value="" aria-label="..."><span>' + txt + '</span><input type="text"><button type="button" class="btn btn-outline-primary edt">Edit</button><button type="button" class="btn btn-outline-primary del">Delete</button></li>');
+            var editBtn = $('<button type="button" class="btn btn-outline-primary edt">Edit</button>')
+            var delBtn = $('<button type="button" class="btn btn-outline-primary del">Delete</button>')
+            lst = $('<li class="list-group-item lis"><input class="form-check-input me-2 mt-2" type="checkbox" value="" aria-label="..."><span>' + txt + '</span><input type="text"></li>');
+            lst.append(editBtn, delBtn);
             $(".tl").append(lst);
+
+            $(editBtn).on('click' , edit);
+            
 
         }
         $("#floatingInput").val("");
@@ -15,7 +41,7 @@ $(document).ready(function () {
         //     let edt = $(this);
         //     et(edt);
         // })
-        // edt = $(".edt");
+        // edt = $(".edt");nclick="myFunction()"
         // et();
         $(".me-2").click(function () {
             if ($(this).is(':checked')) {
@@ -27,31 +53,38 @@ $(document).ready(function () {
             }
         });
 
+
+        // $(".lis").find(".edt").click(function () {
+
+        //     var txt1 = $(this).prev().prev();
+        //     var int = $(this).prev();
+
+        //     console.log(txt1.text());
+        //     console.log(int.val());
+        //     if (int.val() == "") {
+        //         int.val(txt1.text());
+
+        //     }
+        //     else {
+        //         txt1.text(int.val());
+
+        //         int.val("");
+        //     }
+
+        // });
+
         
-        $(".lis").find(".edt").click(function () {
-
-            var txt1 = $(this).prev().prev();
-            var int = $(this).prev();
-
-            console.log(txt1.text());
-            console.log(int.val());
-            if (int.val() == "") {
-                int.val(txt1.text());
-
-            }
-            else {
-                txt1.text(int.val());
-
-                int.val("");
-            }
-
-        });
+        
 
 
         $(".del").click(function () {
             $(this).parent().remove();
         });
     });
+
+
+
+    
 });
 
     // console.log(edt);
